75485dcbd1e7e844b6d34c6dac33b918a954f70d
jilink <jimmyfac@gmail.com>
2022-12-09 17:50:05 +0100

    Exercice 5

--- src/exercices/Exercice5.js
+++ src/exercices/Exercice5.js
@@ -2,12 +2,13 @@ import React  from "react";
 import Exercice from "./Exercice";
 import {Text, Flex } from "@chakra-ui/react";
 
-const consigne = "Dans le fichier src/Exercices/Exercice5.js Complétez la fonction tri pour qu’elle renvoie la liste reçue d’entier triée dans l’ordre croissant Vous ne devez toucher à rien d’autre dans le fichier, vérifiez que la fonction marche dans l’exercice 5 sur votre application React, elle sera updaté à chaque sauvegarde"
+const consigne = "Dans le fichier src/Exercices/Exercice5.js Complétez la fonction tri pour qu’elle renvoie la liste reçue d’entier triée dans l’ordre croissant Vous ne devez toucher à rien d’autre dans le fichier, vérifiez que la fonction marche dans l’exercice 5 sur votre application React, elle sera updaté à chaque sauvegarde ⚠️Attention intediction d’utiliser array.sort();"
 
 function Exercice5() {
   const listInitial = [1, 2, 7, 5, 3, 4, 8, 10, 0]
 
   function tri(list) {
+    // ⚠️Attention intediction d’utiliser array.sort();
     // complétez la fonction tri ici, n'oubliez pas de return la nouvelle liste triée
     return list
   }
